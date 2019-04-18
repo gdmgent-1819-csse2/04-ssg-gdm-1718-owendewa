940e0fd7aae4c8a185cfd75b1450f8fe3b214768
Owen De Waele <owendewa@student.arteveldehs.be>
2019-04-18 14:39:34 +0200

    Update index.vue

--- Documentation_CSSE2/pages/index.vue
+++ Documentation_CSSE2/pages/index.vue
@@ -8,18 +8,7 @@
       <h2 class="subtitle">
         Use nav to see examples
       </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
+      
     </div>
   </section>
 </template>
